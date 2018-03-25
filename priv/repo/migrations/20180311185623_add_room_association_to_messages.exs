f5295a0e3466905e49129678279f3ea44493d53a
Philip Alekseev <phil.alekseev@gmail.com>
2018-03-24 20:59:48 -0400

    Update user association to rooms + add leave room functionality

--- priv/repo/migrations/20180311185623_add_room_association_to_messages.exs
+++ priv/repo/migrations/20180311185623_add_room_association_to_messages.exs
@@ -1,9 +1,20 @@
 defmodule PhoenixChat.Repo.Migrations.AddRoomAssociationToMessages do
   use Ecto.Migration
 
-  def change do
+  def up do
+    execute "ALTER TABLE messages DROP CONSTRAINT messages_user_id_fkey"
     alter table(:messages) do
       add :room_id, references(:rooms, on_delete: :nothing), null: false
+      modify :user_id, references(:users, on_delete: :nothing), null: false
+    end
+    create index(:messages, [:room_id])
+    create index(:messages, [:user_id])
+  end
+
+  def down do
+    execute "ALTER TABLE messages DROP CONSTRAINT messages_user_id_fkey"
+    alter table(:messages) do
+      modify :user_id, references(:users, on_delete: :nothing)
     end
   end
 end
