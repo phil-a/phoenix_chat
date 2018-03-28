ee0bff93b5f3cad702072bdd3e38c6957149984c
Philip Alekseev <phil.alekseev@gmail.com>
2018-03-27 22:31:17 -0400

    Fix room deletion

--- priv/repo/migrations/20180311185623_add_room_association_to_messages.exs
+++ priv/repo/migrations/20180311185623_add_room_association_to_messages.exs
@@ -4,8 +4,8 @@ defmodule PhoenixChat.Repo.Migrations.AddRoomAssociationToMessages do
   def up do
     execute "ALTER TABLE messages DROP CONSTRAINT messages_user_id_fkey"
     alter table(:messages) do
-      add :room_id, references(:rooms, on_delete: :nothing), null: false
-      modify :user_id, references(:users, on_delete: :nothing), null: false
+      add :room_id, references(:rooms, on_delete: :delete_all), null: false
+      modify :user_id, references(:users, on_delete: :delete_all), null: false
     end
     create index(:messages, [:room_id])
     create index(:messages, [:user_id])
