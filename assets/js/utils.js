6cba03420cf79c80bc733ec12123b34faf38efdb
Philip Alekseev <phil.alekseev@gmail.com>
2018-07-10 21:45:59 -0400

    Add happiness activity tab to temporary rooms

---
+++ assets/js/utils.js
@@ -0,0 +1,12 @@
+// Modal functions
+$( "#online-users-button" ).click(function() {
+  $('#onlineUsersModal').modal('toggle');
+});
+
+$( "#add-sticky-reflection-button" ).click(function() {
+  $('#addStickyReflection').modal('toggle');
+});
+
+$( "#add-sticky-happiness-button" ).click(function() {
+  $('#addStickyHappiness').modal('toggle');
+});
